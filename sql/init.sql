adacf22d02c77b8346afe0ca07af18fb364e140f
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 20:23:18 +0200

    Remove OPTIONAL from descr in quizzes table

--- sql/init.sql
+++ sql/init.sql
@@ -10,7 +10,7 @@ create TABLE users (
 CREATE TABLE quizzes (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    descr TEXT OPTIONAL,
+    descr TEXT,
     username VARCHAR(50) NOT NULL,
     PRIMARY KEY (id),
     foreign key (username) references users(username)
