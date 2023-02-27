4872d601de8561d86c78f4677896cb8c697224b9
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 18:08:42 +0200

    Test CRUD functions

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
-require_once "database_connection.php";
+require_once __DIR__ . '/vendor/autoload.php';
 
-echo "Hello";
+require_once "users_functions.php";
+require_once "questions_functions.php";
+require_once "quizzes_functions.php";
+
+var_dump(get_all_users());
+
+echo '<br />';
+
+var_dump(get_all_quizzes());
+
+echo '<br />';
+
+var_dump(get_all_questions());
