7dd75a9b506de1ecf7e6be141d506b7658e112ee
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 20:18:31 +0200

    Reformat

--- database/functions/users_functions.php
+++ database/functions/users_functions.php
@@ -9,7 +9,8 @@ function select_all_users() {
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function insert_user($username, $email, $password) {
+function insert_user($username, $email, $password)
+{
     global $conn;
 
     $query = "INSERT INTO users (username, email, passwd) VALUES ('$username', '$email', '$password');";
