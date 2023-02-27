30efc07a641df54fe65ea82f55874a5d57fe9378
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 23:02:05 +0200

    Move error messages to a separate file

--- utilities.php
+++ utilities.php
@@ -10,3 +10,11 @@ function is_authenticated()
 
     return username_exists($_SESSION['username']);
 }
+
+$error_messsages = array(
+    'emptyFields' => 'Please fill out all the fields',
+    'invalidEmail' => 'Please enter a valid email address',
+    'invalidUser' => 'Invalid username or password',
+    'usernameTaken' => 'This usersname is used by another user, please try another',
+    'emailTaken' => 'This email is used by another user, please try another'
+);
