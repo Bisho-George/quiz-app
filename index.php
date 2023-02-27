c1b351ed5e7699ec4369c4f932d7d1b326aa1d5c
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 20:18:07 +0200

    Redirect to login page

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once "utilities.php";
 
-require_once "users_functions.php";
-require_once "questions_functions.php";
-require_once "quizzes_functions.php";
-
-var_dump(get_all_users());
-
-echo '<br />';
-
-var_dump(get_all_quizzes());
-
-echo '<br />';
-
-var_dump(get_all_questions());
+redirect('login.php');
