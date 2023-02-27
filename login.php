1d3bb40448a4ef025f8fce0b2f3f9da374f42e32
youssef-attai <youssefgalalnazem@gmail.com>
2023-02-27 23:04:43 +0200

    Load input values from query params

--- login.php
+++ login.php
@@ -1,16 +1,28 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
+    <p>
+        <?php
+        require_once "utilities.php";
+
+        key_exists('error', $_GET) ? print $error_messsages[$_GET['error']] : print '';
+        ?>
+    </p>
     <form action="/routes/auth/login.php" method="post">
-        <input type="text" name="username" placeholder="username">
-        <input type="password" name="password" placeholder="password">
+        <input required type="text" placeholder="username" name="username" value="<?php
+            key_exists('username', $_GET) ? print $_GET['username'] : print '';
+        ?>">
+        <input required type="password" placeholder="password" name="password">
         <button type="submit">login</button>
     </form>
     <a href="register.php">don't have an account? click here to register.</a>
 </body>
+
 </html>
