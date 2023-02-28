20dcd022206eaf985a016b9acbed963e5457d9f6
bisho515 <bishogeorge689@gmail.com>
2023-02-28 16:51:04 +0200

    modifying in quiz table and index file

--- index.php
+++ index.php
@@ -4,3 +4,17 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once "database_connection.php";
 
 echo "Hello";
+// CRUD Operations
+
+// Create Quiz
+function createQuiz ($title, $description, $userId) {
+    $sql = "INSERT INTO quiz (title, _description, username) VALUES ('$title', '$description', '$userId')";
+    $result = mysqli_query($GLOBALS['conn'], $sql);
+    if ($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+?>
