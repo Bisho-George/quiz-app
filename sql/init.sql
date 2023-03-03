5466613c4bbcaedb6a835deae4c2ca5ea9e9fc4e
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-03 10:20:28 +0200

    Add is_correct to answer schema

--- sql/init.sql
+++ sql/init.sql
@@ -26,6 +26,7 @@ CREATE TABLE answers(
     id INT NOT NULL AUTO_INCREMENT,
     answer VARCHAR(255) NOT NULL,
     question_id INT NOT NULL,
+    is_correct BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
