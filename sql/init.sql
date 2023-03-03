f4d4c1e90aeee75b4783d77f63db1dfe78fe966d
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-03 10:19:58 +0200

    Remove question type from question schema

--- sql/init.sql
+++ sql/init.sql
@@ -18,7 +18,6 @@ CREATE TABLE quizzes (
 CREATE TABLE questions(
     id INT NOT NULL AUTO_INCREMENT,
     question VARCHAR(255) NOT NULL,
-    question_type ENUM('mcq', 'tf') NOT NULL,
     quiz_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
